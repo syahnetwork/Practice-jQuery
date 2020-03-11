a0449fab4db8b3e6c7ab53b301b0006732184b21
adam <syahnetwork@gmail.com>
2020-03-11 08:29:07 +0700

    fading transitions

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
 $(document).ready(function () {
-  // $('.hide').click(function () {
-  //   $('.element').hide(2000, function () {
-  //     alert('done')
-  //   })
-  // })
-
-  // $('.show').click(function () {
-  //   $('.element').show()
-  // })
-
-
-  $('.hide,.show').click(function () {
-    //toggle visible or not visible
-    // $('.element').toggle(2000, function () {
-    // $('.element').toggle('slow', function () {
-    $('.element').toggle('fast', function () {
+  $('.fadeIn').click(function () {
+    $('.element').fadeIn(2000, function () {
       alert('done')
     })
   })
+
+  $('.fadeOut').click(function () {
+    $('.element').fadeOut(2000, function () {
+      alert('done')
+    })
+  })
+
+  $('.fadeIn,.fadeOut').click(function () {
+    $('.element').fadeToggle('fast', function () {
+      alert(done)
+    })
+  })
+
+  $('.fadeOut').click(function () {
+    $('.element').fadeTo('slow',0.33,function(){ //until 0.33 opacity
+      alert('done') //callback
+    })
+  })
+
 
 })
 
