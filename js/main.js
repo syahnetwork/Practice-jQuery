7a26be5629b0aa3ac46a5d570c0c6f8fe168519d
adam <syahnetwork@gmail.com>
2020-03-11 08:33:53 +0700

    slide transitions

--- js/main.js
+++ js/main.js
@@ -1,25 +1,19 @@
 $(document).ready(function () {
-  $('.fadeIn').click(function () {
-    $('.element').fadeIn(2000, function () {
+  // $('.slideUp').click(function () {
+  //   $('.element').slideUp(2000, function () {
+  //     alert(done)
+  //   })
+  // })
+
+  // $('.slideDown').click(function () {
+  //   $('.element').slideDown(2000, function () {
+  //     alert(done)
+  //   })
+  // })
+
+  $('.slideUp,.slideDown').click(function () {
+    $('.element').slideToggle('slow', function () {//no slideto
       alert('done')
-    })
-  })
-
-  $('.fadeOut').click(function () {
-    $('.element').fadeOut(2000, function () {
-      alert('done')
-    })
-  })
-
-  $('.fadeIn,.fadeOut').click(function () {
-    $('.element').fadeToggle('fast', function () {
-      alert(done)
-    })
-  })
-
-  $('.fadeOut').click(function () {
-    $('.element').fadeTo('slow',0.33,function(){ //until 0.33 opacity
-      alert('done') //callback
     })
   })
 
