28fdcd014263dcab99d9046e9bf3c5da2aa607f3
adam <syahnetwork@gmail.com>
2020-03-11 09:00:18 +0700

    stop animation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 $(document).ready(function () {
-  $('.element').click(function () {
-    $(this).animate({
-      left: '200px',
-      top: '50px',
-      width: '100px',
-      height: '100px',
-      fontSize: '3em',
-      opacity: '0.2'
-    // },2000,'easeOutBounce')
-    // },2000,'easeInBack')
-    },2000,'easeInBack',function(){
-      alert('animate done :))')
-    })
+
+  //need second library because jquery doesn't include it, jquery UI
+  //toggle needed library at jquery UI download page
+  //or use jquery UI cdn
+
+  // $('.element').hover(function () {
+  //   $(this).animate({ left: '100px' }, 'slow')
+  // }, function () {
+  //   $(this).animate({ left: '0px' }, 'slow') //not use in real because when you hover , animate will run unexpectedly
+  // })
+
+  $('.element').hover(function () {
+    $(this).stop().animate({ left: '100px' }, 'slow')
+  }, function () {
+    $(this).stop().animate({ left: '0px' }, 'slow') //stop the queque animation
   })
-//need second library because jquery doesn't include it, jquery UI
-//toggle needed library at jquery UI download page
-//or use jquery UI cdn
 })
 
 
