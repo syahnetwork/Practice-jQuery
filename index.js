266fa260f4b55eb0ea0455ebbae29f978c0c76b5
Adam <syahnetwork@gmail.com>
2020-03-31 14:34:31 +0700

    creating node server

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var app = express();
 var port = 3000;
 var path = require("path");
 
-app.set("view options",{layout:false});
-app.use(express.static(path.join(__dirname,'public')));
+app.set("view options", { layout: false });
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/public/index.html'))
+app.get('/', function (req, res) {//routes
+  res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
-app.listen(port,function(){
-    console.log('server running')
+app.listen(port, function () {
+  console.log('server running')
 })
+//restart server to grab changes
+//node modules  - npm install
